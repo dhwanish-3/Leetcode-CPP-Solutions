be554e5acb61c2bcf7dae42018bd93f99ed1f118
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2023-06-01 17:44:04 +0530

    renamed:    1091ShortestPathInBinaryMatrix.cpp -> 1091-ShortestPathInBinaryMatrix.cpp 	renamed:    RomanToInt.cpp -> 13-RomanToInt.cpp 	renamed:    1396UndegroundSystem.cpp -> 1396-UndegroundSystem.cpp 	renamed:    parkingSystemEasy.cpp -> 1603-parkingSystemEasy.cpp 	renamed:    705designHashSet.cpp -> 705-designHashSet.cpp

--- 1091-ShortestPathInBinaryMatrix.cpp
+++ 1091-ShortestPathInBinaryMatrix.cpp
@@ -6,8 +6,8 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
         if(n==1 && grid[0][0]==0) return 1;
-        if(grid[n-1][n-1] || grid[0][0]==1) return -1;
-        const vector<pair<int,int>> directions={{-1,-1},{-1,0},{-1,1},{0,-1},{0,0},{0,1},{1,-1},{1,0},{1,1}};
+        if(grid[0][0]==1 || grid.back().back()==1) return -1;
+        const vector<pair<int,int>> directions={{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
         queue<pair<int,int>> q{{{0,0}}};
         vector<vector<bool>> visited(n,vector<bool>(n));
         visited[0][0]=true;
@@ -15,16 +15,25 @@ public:
         while(!q.empty()){
             ++pathLength;
             int i,j,dx,dy;
-            for(int x=q.size();x>0;x--){
+            for(int size=q.size();size>0;--size){
                 i=q.front().first; 
                 j=q.front().second; 
                 q.pop();
-                for(int d=0;d<9;d++){
-                    dx=directions[d].first;
-                    dy=directions[d].second;
+                for(auto d=directions.begin();d!=directions.end();++d){
+                    auto& dir=*d;
+                    dx=dir.first;
+                    dy=dir.second;
+                    int x=i+dx;
+                    int y=i+dy;
+                    if(x<0 || x==n || y<0 || y==n) continue;
+                    if(grid[x][y]!=0 || visited[x][y]) continue;
+                    if(x==n-1 && y==n-1) return pathLength+1;
+                    q.emplace(x,y);
+                    visited[x][y]=true;
                 }
             }
         }
+        return -1;
     }
 };
 
@@ -41,6 +50,6 @@ int main(){
         }
         binaryMatrix.push_back(row);
     }
-    
+    cout<<Solution().shortestPathBinaryMatrix(binaryMatrix);
     return 0;
 }
