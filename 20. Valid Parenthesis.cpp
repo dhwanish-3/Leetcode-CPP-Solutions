4a170d36aff02820728aa1d1422f85d9983ed0dd
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2023-06-04 06:59:10 +0530

    new file:   20. Valid Parenthesis.cpp

---
+++ 20. Valid Parenthesis.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    char pair(char c){
+        switch(c){
+            case ')': return  '(';
+            case ']': return  '[';
+            case '}': return  '{';
+        }
+        return ' ';
+    }
+    bool isOpening(char c){
+        switch(c){
+            case '(': return true;
+            case '[': return true;
+            case '{': return true;
+        }
+        return false;
+    }
+    bool isValid(string s) {
+        stack<char> stk;
+        for(int i=0;i<s.length();i++){
+            if(isOpening(s[i])){
+                stk.push(s[i]);
+            }else{
+                if(stk.empty()){
+                    return false;
+                }
+                else if(stk.top()==pair(s[i])){
+                    stk.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+int main(){
+    string str;
+    cin>>str;
+    cout<<Solution().isValid(str);
+    return 0;
+}
