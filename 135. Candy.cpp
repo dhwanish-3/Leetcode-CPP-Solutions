b2ebaa5920cc644cd27a7cf8dbcd0a0c608c1ece
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2023-09-13 20:05:10 +0530

    GREEDY IN DAA

---
+++ 135. Candy.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> candies(n, 1);
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += candies[i];
+        }
+        return sum;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> ratings(n);
+    for (int i = 0; i < n; i++) {
+        cin >> ratings[i];
+    }
+    cout << Solution().candy(ratings) << endl;
+    return 0;
+}
