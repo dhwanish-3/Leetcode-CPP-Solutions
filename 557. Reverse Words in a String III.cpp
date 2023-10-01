93e22c7bc26da8dca37ad2d60dca823e2c766e3a
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2023-10-01 21:07:19 +0530

    1D  DP easy

---
+++ 557. Reverse Words in a String III.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int l = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == ' ') {
+                reverse(s.begin() + l, s.begin() + i);
+                l = i + 1;
+            }
+        }
+        reverse(s.begin() + l, s.end());
+        return s;
+    }
+};
+
+int main() {
+    string s;
+    getline(cin, s);
+    cout << Solution().reverseWords(s) << endl;
+    return 0;
+}
--- 746. Min Cost Climbing Stairs.cpp
+++ 746. Min Cost Climbing Stairs.cpp
@@ -4,12 +4,14 @@ using namespace std;
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        int n=cost.size();
-        vector<int> dp(n+2,0);
-        for(int i=n-1;i>=0;i--){
-            dp[i]=cost[i]+min(dp[i+1],dp[i+2]);
+        int n = cost.size();
+        int dp[n + 2];
+        dp[n + 1] = 0;
+        dp[n] = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = cost[i] + min(dp[i + 1], dp[i + 2]);
         }
-        return min(dp[0],dp[1]);
+        return min(dp[0], dp[1]);
     }
 };
 
