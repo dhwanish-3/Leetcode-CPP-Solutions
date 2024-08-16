117b0451e0ce5d9306dfe1140b3206dab94ccbef
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2024-08-16 11:55:02 +0530

    modi

--- 134. Gas Station.cpp
+++ 134. Gas Station.cpp
@@ -7,31 +7,31 @@ public:
         int n = gas.size();
         int gasSum = 0;
         int totalCost = 0;
-        for(int i=0 ; i<n ; i++){
+        for(int i = 0; i < n; i++){
             gasSum += gas[i];
             totalCost += cost[i];
         }
         if(gasSum < totalCost) return -1;
         int start = 0;
         int sum = 0;
-        for(int i=0 ; i<n ; i++){
+        for(int i = 0; i < n; i++){
             sum += gas[i] - cost[i];
             if(sum < 0){
                 sum = 0;
-                start = i+1;
+                start = i + 1;
             }
         }
         return start;
     }
 };
 
-int main(){
+int main() {
     int n;
-    cin>>n;
+    cin >> n;
     vector<int> gas(n);
     vector<int> cost(n);
-    for(int i=0;i<n;i++) cin>>gas[i];
-    for(int i=0;i<n;i++) cin>>cost[i];
-    cout<<Solution().canCompleteCircuit(gas,cost);
+    for(int i = 0; i < n; i++) cin >> gas[i];
+    for(int i = 0; i < n; i++) cin >> cost[i];
+    cout << Solution().canCompleteCircuit(gas, cost);
     return 0;
 }
