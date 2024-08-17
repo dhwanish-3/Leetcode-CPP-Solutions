755a404ee64f3b2b8819abd4aba3598d0a58a077
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2024-08-17 23:58:46 +0530

    h

--- 211. Design Add and Search Words Data Structure.cpp
+++ 211. Design Add and Search Words Data Structure.cpp
@@ -1,56 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct TrieNode {
-    vector<shared_ptr<TrieNode>> children;
-    bool isWord = false;
-    TrieNode() : children(26) {}
+class TrieNode {
+public:
+    vector<TrieNode*> data;
+    bool isEndOfWord = false;
+    TrieNode() {
+        data.resize(26);
+    }
 };
 
 class WordDictionary {
-    shared_ptr<TrieNode> root = make_shared<TrieNode> ();
-    bool dfs(string& word, int s, shared_ptr<TrieNode> node) {
-        if (s == word.length()) {
-            return node->isWord;
-        }
-        if (word[s] != '.') {
-            shared_ptr<TrieNode> next = node->children[word[s] - 'a'];
-            return next ? dfs(word, s+1, next) : false;
-        }
-        // word[s] = '.' -> search all 26 children
-        for (int i=0 ; i<26 ; i++) {
-            if (node->children[i] && dfs(word, s+1, node->children[i])) {
-                return true;
-            }
-        }
-        return false;
-    }
+    TrieNode* root;
 public:
     WordDictionary() {
-        
+        root = new TrieNode();
     }
     
     void addWord(string word) {
-        shared_ptr<TrieNode> node = root;
-        for (char c: word) {
-            int i = c -'a';
-            if (node->children[i] == nullptr) {
-                node->children[i] = make_shared<TrieNode> ();
+        TrieNode* cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            int j = word[i] - 'a';
+            if (!cur->data[j]) {
+                cur->data[j] = new TrieNode();
             }
-            node = node->children[i];
+            cur = cur->data[j];
         }
-        node->isWord = true;
+        cur->isEndOfWord = true;
+    }
+
+    bool rec(string word, TrieNode* cur, int i) {
+        if (word.length() == i) {
+            return cur->isEndOfWord;
+        }
+        if (word[i] == '.') {
+            for (int j = 0; j < 26; j++) {
+                if (cur->data[j]) {
+                    if (rec(word, cur->data[j], i + 1)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        } else {
+            int ch = word[i] - 'a';
+            if (cur->data[ch] == nullptr) {
+                return false;
+            }
+            return rec(word, cur->data[ch], i + 1);
+        }
     }
     
     bool search(string word) {
-        return dfs(word, 0 ,root);   
+        return rec(word, root, 0);
     }
 };
 
-int main(){
+int main() {
     WordDictionary* obj = new WordDictionary();
     obj->addWord("word");
-    cout<<obj->search("word");
-    
+    cout << obj->search("word");
     return 0;
 }
